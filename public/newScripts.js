7dd0af63950d6d6f94c09fa38d316c1f7822fa90
Aram Hekimian <guerrerohek121@gmail.com>
2025-01-19 15:34:17 -0600

    Implemented admin pass check to new movie form

--- public/newScripts.js
+++ public/newScripts.js
@@ -14,27 +14,27 @@ if (selected.length >= maxSelection) {
   });
 }
 
-// const overlay = document.createElement("div");
-// overlay.style.position = "absolute";
-// overlay.style.top = 0;
-// overlay.style.left = 0;
-// overlay.style.width = "100%";
-// overlay.style.height = "100%";
-// overlay.style.backgroundColor = "rgba(0, 0, 0, .3)"; // Transparent
-// newCont.appendChild(overlay);
+const overlay = document.createElement("div");
+overlay.style.position = "absolute";
+overlay.style.top = 0;
+overlay.style.left = 0;
+overlay.style.width = "100%";
+overlay.style.height = "100%";
+overlay.style.backgroundColor = "rgba(0, 0, 0, .3)"; // Transparent
+newCont.appendChild(overlay);
 
-// const adminPassword = document.querySelector(".admin--input");
-// const enterBtn = document.querySelector(".pass--btn");
+const adminPassword = document.querySelector(".admin--input");
+const enterBtn = document.querySelector(".pass--btn");
 
-// adminPassword.addEventListener("input", () => {
-//   if (adminPassword.value === "670222") {
-//     overlay.style.display = "none";
-//     adminPassword.parentElement.style.display = "none";
-//     enterBtn.diabled = false;
-//   } else {
-//     enterBtn.disabled = true;
-//   }
-// });
+adminPassword.addEventListener("input", () => {
+  if (adminPassword.value === "670222") {
+    overlay.style.display = "none";
+    adminPassword.parentElement.style.display = "none";
+    enterBtn.diabled = false;
+  } else {
+    enterBtn.disabled = true;
+  }
+});
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener("change", () => {
